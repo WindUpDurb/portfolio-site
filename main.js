7479cff4b16b49fba001fbddc6fc7cfb8c5a3b02
David Urbina <durbina1991@gmail.com>
2016-11-19 21:21:57 -0800

    More Updates

--- main.js
+++ main.js
@@ -3,6 +3,11 @@
 const linkedInIcon = document.getElementById("linkedInIcon");
 const twitterIcon = document.getElementById("twitterIcon");
 const githubIcon = document.getElementById("githubIcon");
+const openResume = document.getElementById("openResume");
+const dddDeployedHere = document.getElementById("dddDeployedHere");
+const jsmDeployedHere = document.getElementById("jsmDeployedHere");
+const dddCodeHere = document.getElementById("dddCodeHere");
+const jsmCodeHere = document.getElementById("jsmCodeHere");
 
 linkedInIcon.addEventListener("click", () => {
     window.open("https://www.linkedin.com/in/david-urbina-589327b9", "_blank");
@@ -14,4 +19,25 @@ twitterIcon.addEventListener("click", () => {
 
 githubIcon.addEventListener("click", () => {
     window.open("https://github.com/windupdurb", "_blank");
+});
+
+openResume.addEventListener("click", () => {
+   window.open("/resume.pdf", "_blank");
+});
+
+dddDeployedHere.addEventListener("click", () => {
+    window.open("http://www.drinkdrankdrunk.tech/", "_blank");
+});
+
+dddCodeHere.addEventListener("click", () => {
+    window.open("https://github.com/WindUpDurb/drink-drank-drunk-react", "_blank");
+});
+
+
+jsmDeployedHere.addEventListener("click", () => {
+    window.open("http://www.jsm-app.tech/", "_blank");
+});
+
+jsmCodeHere.addEventListener("click", () => {
+    window.open("https://github.com/amazingandyyy/jobrm", "_blank");
 });
